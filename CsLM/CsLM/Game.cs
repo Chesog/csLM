206d0ec46f4cdff76074b65495c951b81ff126ad
Dario <otrantodario@gmail.com>
2021-06-14 20:33:00 -0300

    intento de lectura de archivos.txt

--- CsLM/CsLM/Game.cs
+++ CsLM/CsLM/Game.cs
@@ -3,17 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 
 
 class Game
 {
+    
     public Game()
     {
+        Player player = new Player();
+        Village village = new Village();
+        Gameplay gameplay = new Gameplay();
+        Shop shop = new Shop();
 
     }
     public bool Menu()
     {
+        try
+        {
+            StreamReader sr = new StreamReader("./Menuintro/menu");
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            Console.ReadLine();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+            
+        }
+        finally
+        {
+            Console.WriteLine("Executing finally block.");
+        }
+
+
         return true;
     }
 }
